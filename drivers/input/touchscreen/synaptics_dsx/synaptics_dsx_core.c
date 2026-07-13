c249511205653b4507efc8b60e2eacad85250eb5
sunflowers.warp@gmail.com <sunflowers.warp@gmail.com>
2026-07-13 16:49:17 +0300

    ci

--- drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_core.c
+++ drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_core.c
@@ -4336,7 +4336,7 @@ static int synaptics_rmi4_probe(struct platform_device *pdev)
 
 #ifdef CONFIG_FB
 	rmi4_data->fb_notifier.notifier_call = synaptics_rmi4_dsi_panel_notifier_cb;
-	retval = msm_drm_register_client(&rmi4_data->fb_notifier);
+	retval = fb_register_client(&rmi4_data->fb_notifier);
 	if (retval < 0) {
 
 
@@ -4444,7 +4444,7 @@ err_virtual_buttons:
 
 err_enable_irq:
 #ifdef CONFIG_FB
-	msm_drm_unregister_client(&rmi4_data->fb_notifier);
+	fb_unregister_client(&rmi4_data->fb_notifier);
 #endif
 
 #ifdef USE_EARLYSUSPEND
@@ -4531,7 +4531,7 @@ static int synaptics_rmi4_remove(struct platform_device *pdev)
 	synaptics_rmi4_irq_enable(rmi4_data, false, false);
 
 #ifdef CONFIG_FB
-	msm_drm_unregister_client(&rmi4_data->fb_notifier);
+	fb_unregister_client(&rmi4_data->fb_notifier);
 #endif
 
 #ifdef USE_EARLYSUSPEND
@@ -4578,7 +4578,7 @@ static int synaptics_rmi4_dsi_panel_notifier_cb(struct notifier_block *self,
 		unsigned long event, void *data)
 {
 	int transition;
-	struct msm_drm_notifier *evdata = data;
+	struct fb_event *evdata = data;
 	struct synaptics_rmi4_data *rmi4_data =
 			container_of(self, struct synaptics_rmi4_data,
 			fb_notifier);
@@ -4587,12 +4587,12 @@ static int synaptics_rmi4_dsi_panel_notifier_cb(struct notifier_block *self,
 		return 0;
 
 	if (evdata && evdata->data && rmi4_data) {
-		if (event == MSM_DRM_EVENT_BLANK) {
+		if (event == FB_EVENT_BLANK) {
 			transition = *(int *)evdata->data;
-			if (transition == MSM_DRM_BLANK_POWERDOWN) {
+			if (transition == FB_BLANK_POWERDOWN) {
 				synaptics_rmi4_suspend(&rmi4_data->pdev->dev);
 				rmi4_data->fb_ready = false;
-			} else if (transition == MSM_DRM_BLANK_UNBLANK) {
+			} else if (transition == FB_BLANK_UNBLANK) {
 				synaptics_rmi4_resume(&rmi4_data->pdev->dev);
 				rmi4_data->fb_ready = true;
 			}
